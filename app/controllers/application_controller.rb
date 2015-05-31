de9a5080ee1ccb7a5c412764d87816fcb84c425f
Zoee Silcock <mrzoee@gmail.com>
2015-05-31 17:08:34 +0200

    Use basic auth for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    if ENV['PASSWORD'] && controller_name != 'home'
+      unless authenticate_with_http_basic { |u, p| p == ENV['PASSWORD'] }
+        request_http_basic_authentication
+      end
+    end
+  end
 end
